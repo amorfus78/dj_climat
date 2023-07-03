9a8c75e8f80a7b17bedeb093b6028149d8a7c7c8
Etienne Dubois <etiennedubois@gmail.com>
2023-07-03 12:16:09 +0200

    change to module

--- api/app.js
+++ api/app.js
@@ -1,35 +1,40 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+import createError from "http-errors"
+import express from "express"
+import path from "path"
+import cookieParser from "cookie-parser"
+import { fileURLToPath } from "url"
+import logger from "morgan"
 
-var app = express();
+var app = express()
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
+const __filename = fileURLToPath(import.meta.url)
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+const __dirname = path.dirname(__filename)
+
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "pug")
+
+app.use(logger("dev"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, "public")))
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
-});
+  next(createError(404))
+})
 
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get("env") === "development" ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
+  res.status(err.status || 500)
+  res.render("error")
+})
 
-module.exports = app;
+export default app
